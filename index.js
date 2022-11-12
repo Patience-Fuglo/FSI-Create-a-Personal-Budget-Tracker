39162023614718d6a77576452b0ba93f3f9cbf9e
Patience-Fuglo <pfuglo@fordham.edu>
2022-11-12 16:15:03 -0500

    index.js

--- index.js
+++ index.js
@@ -3,3 +3,48 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+
+let weeklyExpenses = 0
+let monthlyExpenses = 0
+let annualExpenses = 0
+
+// Your Code Here
+
+window.prompt(weeklyExpenseQuestions[0])
+let answer = window.prompt(weeklyExpenseQuestions[0])
+console.log(answer)
+
+let stringAnswer = window.prompt(weeklyExpenseQuestions[0])
+let numberAnswer = parseFloat(stringAnswer)
+console.log(numberAnswer)
+
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+console.log(answer)
+
+let weeklyExpenses = 0
+let monthlyExpenses = 0
+let annualExpenses = 0
+
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+weeklyExpenses = weeklyExpenses + answer
+
+
+let weeklyExpenses = 0
+let monthlyExpenses = 0
+let annualExpenses = 0
+
+for((let i = 0; i < weeklyExpenseQuestions.length; ){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+
+for(let i = 0; i < weeklyExpenseQuestions.length; ){
+  let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+  weeklyExpenses = weeklyExpenses + answer
+}
+
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+  let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+  weeklyExpenses = weeklyExpenses + answer
+}
+
